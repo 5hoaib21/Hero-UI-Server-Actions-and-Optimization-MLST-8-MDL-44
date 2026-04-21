8e07d9b54ff3e61a3ed36ba7943bcf9ec8755bb1
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-04-22 01:22:15 +0600

    Revalidation after server action to display form data

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,6 +1,25 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
+
 export const createATask = async (formData) => {
   "use server";
-  const name = formData.get("name");
 
-  console.log("adding a task with name:", formData);
+  // const title = formData.get("title");
+  // const description = formData.get("description");
+  // const priority = formData.get("priority");
+  // const status = formData.get("status");
+  // const assignedTo = formData.get("assignedTo");
+
+  // const newTask = { title, description, priority, status, assignedTo };
+  const newTask = Object.fromEntries(formData.entries());
+
+  console.log("adding a task with name:", newTask);
+
+  const res = await postTask(newTask);
+
+  if (res.ok) {
+    revalidatePath("/tasks");
+  }
+
+  return res;
 };
